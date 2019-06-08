dbb65452506280862f6e0c4d5b1a19ddb60f1531
Giuliana <giulianaolmos@gmail.com>
2019-06-08 16:03:53 -0300

    Test del reservarHorarios cantidad

--- js/tests.js
+++ js/tests.js
@@ -7,14 +7,29 @@ describe('Test de Reserva Horario', function(){
 		nuevoRestaurant.reservarHorario("13:00");
 		expect(nuevoRestaurant.horarios).eql(["15:30", "18:00"]);
 	})
+	it('Disminuye la cantidad de elementos', function(){
+		var nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+		nuevoRestaurant.reservarHorario("13:00");
+		expect(nuevoRestaurant.horarios.length).to.equal(2);
+	})
 	it('No hay cambios al reservar un horario inexistente', function(){
 		var nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
 		nuevoRestaurant.reservarHorario("10:00");
 		expect(nuevoRestaurant.horarios).eql(["13:00", "15:30", "18:00"]);
 	})
+	it('La cantidad de horarios se mantiene cuando el horario selecicionado no existe', function(){
+		var nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+		nuevoRestaurant.reservarHorario("11:00");
+		expect(nuevoRestaurant.horarios.length).to.equal(3);
+	})
 	it('No hay cambios al pasar un parametro vacio', function(){
 		var nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
 		nuevoRestaurant.reservarHorario();
 		expect(nuevoRestaurant.horarios).eql(["13:00", "15:30", "18:00"]);
+	})
+	it('La cantidad de horarios se mantiene cuando el parametro esta vacio', function(){
+		var nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+		nuevoRestaurant.reservarHorario();
+		expect(nuevoRestaurant.horarios.length).to.equal(3);
 	})
 });
